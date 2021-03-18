fd597dc73f886901695acf0e520d514fa14bf3a0
joshuamschmidt <joshmschmidt1@gmail.com>
2021-03-19 10:04:55 +1030

    first yaml dump!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # conda_yamls
 place to store and version control my conda environments
+
+#### Little tips
+
+conda base dir: conda info | grep -i 'base environment'
+
+
--- yaml_dump.sh
+++ yaml_dump.sh
@@ -1,11 +1,14 @@
-#!/usr/local/bash
+#!/usr/bin/env bash
 
-yaml_dir="~/Projects/conda_yamls"
+#yaml_dir="~/Projects/conda_yamls"
 # assume all envs can be activated by name: https://stackoverflow.com/questions/57527131/conda-environment-has-no-name-visible-in-conda-env-list-how-do-i-activate-it-a
+source /Users/joshuaschmidt/anaconda3/etc/profile.d/conda.sh
 
-for env in $(conda env list | cut -d" " -f1 | tail -n+4 | awk 'length($0) > 1'); 
-do conda activate $env;
-conda env export > ${yaml_dir}/${env}.yml;
+sleep 5
+
+for envirnonment in $(conda env list | cut -d" " -f1 | tail -n+4 | awk 'length($0) > 1'); 
+do conda activate $envirnonment;
+conda env export > ${envirnonment}.yml;
 conda deactivate;
 done
 
